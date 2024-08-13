01df105ab4ef6ebbdd8d6395a081850cd5e6811f
H3xWizz <ksawier.malkiewicz@icloud.com>
2024-08-13 11:41:52 +0200

    feat: add mobile view

---
+++ src/components/mobile-info.tsx
@@ -0,0 +1,7 @@
+export default function MobileInfo() {
+    return (
+        <div className={'h-screen w-screen bg-background fixed flex justify-center items-center text-center top-0 left-0 z-50 md:hidden'}>
+            <p>Please run app on desktop<br/>sorry for the inconvenience :(</p>
+        </div>
+    )
+}
