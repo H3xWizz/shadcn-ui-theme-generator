81566f7e0d7be1848a5ebf3531f7002b90c9afd3
H3xWizz <ksawier.malkiewicz@icloud.com>
2024-08-13 11:49:15 +0200

    feat: add copyrights and gh link on right bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Header from "@/components/header";
 import ExamplesView from "@/components/examples-view";
 import ColorsChanger from "@/components/colors-changer";
+import Link from "next/link";
+import {GithubIcon} from "lucide-react";
 
 export default function Home() {
     return (
@@ -11,6 +13,12 @@ export default function Home() {
             </div>
             <div className={'col-span-1 row-span-2 pb-12'}>
                 <ColorsChanger/>
+            </div>
+            <div className={'fixed right-4 bottom-2 text-muted text-sm flex gap-2 items-center'}>
+                <Link href={'https://github.com/H3xWizz'}>
+                    <GithubIcon size={16} className={'transition-all hover:text-white'}/>
+                </Link>
+                <p>H3xWizz &copy; 2024</p>
             </div>
         </div>
     );
